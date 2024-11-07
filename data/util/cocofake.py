19556c8b644ec107387082d69c9008381889d5af
Michael Lee <leemd1@leidos.com>
2024-11-06 21:42:23 -0500

    Added test split to dataloader util

--- data/util/cocofake.py
+++ data/util/cocofake.py
@@ -6,13 +6,30 @@ import numpy as np
 from PIL import Image
 
 class CocoFake(torch.utils.data.Dataset):
-    def __init__(self, coco_path, cocofake_path, transforms=None, split="train", limit=-1):
-        self.cocofake_path = os.path.join(cocofake_path, f"{split}2014")
-        if split == "val":  # coco-2014 val folder is called validation while cocofake is called val2014
-            split = "validation"
-        self.coco_path = os.path.join(coco_path, split, "data")
+    def __init__(
+        self,
+        coco_path,
+        cocofake_path,
+        real_transform=None,
+        fake_transform=None,
+        split="train",
+        limit=-1,
+        test_split=None,
+    ):
+        if split == "test" or split == "val":
+            self.cocofake_path = os.path.join(cocofake_path, "val2014")
+            self.coco_path = os.path.join(coco_path, "validation", "data")
+        else:
+            self.cocofake_path = os.path.join(cocofake_path, f"train2014")
+            self.coco_path = os.path.join(coco_path, "train", "data")
 
         self.real_images = sorted(os.listdir(self.coco_path))[:limit]
+        if test_split is not None:
+            n = len(self.real_images) // 2
+            if split == "test":
+                self.real_images = self.real_images[n:]
+            else:
+                self.real_images = self.real_images[:n]
         self.transforms = transforms
 
     def __len__(self):
@@ -30,9 +47,10 @@ class CocoFake(torch.utils.data.Dataset):
             for x in fake_image_paths
         ]
 
-        if self.transforms is not None:
-            real_image = self.transforms(real_image)
-            fake_images = [self.transform(x) for x in fake_images]
+        if self.real_transform is not None:
+            real_image = self.real_transform(real_image)
+        if self.fake_transform is not None:
+            fake_images = [self.fake_transform(x) for x in fake_images]
 
         real_image = np.transpose(real_image, (2, 0, 1)).astype(np.float32)
         fake_images = [np.transpose(x, (2, 0, 1)).astype(np.float32) for x in fake_images]
@@ -40,18 +58,57 @@ class CocoFake(torch.utils.data.Dataset):
         return {"real": real_image, "fake": fake_images}
 
 
-def get_cocofake(coco_path, cocofake_path, transforms=None, batch_size=1, n=-1):
+def get_cocofake(
+    coco_path,
+    cocofake_path,
+    transforms=None,
+    batch_size=1,
+    train_limit=-1,
+    val_limit=-1,
+    test_split=0.5,
+):
+    """
+    Retrieve train, validation, and test dataloaders.
+
+    Arguments
+    ---------
+    coco_path : str
+        Path to the coco-2014 root directory
+    cocofake_path : str
+        Path to the cocofake root directory
+    transforms : callable
+        Transformation functions to preprocess the images
+    batch_size : int
+        Batch size
+    train_limit : int
+        Maximum number of images to include in the training set
+    val_limit : int
+        Maximum number of images to include in the validation set
+    test_split : float
+        Proportion of the validation set to use as the test set.
+        Note: the test set is split after val_limit is applied.
+        For example, if val_limit=10000 and test_split=0.5, then the validation set will have
+        5000 images and the test set will have 5000 images.
+
+    Returns
+    -------
+    train : torch.util.data.DataLoader
+    val : torch.util.data.DataLoader
+    test : torch.util.data.DataLoader
+    """
     if not os.path.exists(coco_path):
         import fiftyone
         coco = fiftyone.zoo.load_zoo_dataset("coco-2014")
 
     assert os.path.exists(cocofake_path)
 
-    train = CocoFake(coco_path, cocofake_path, transforms=transforms, split="train", limit=n)
-    val = CocoFake(coco_path, cocofake_path, transforms=transforms, split="val", limit=n)
+    train = CocoFake(coco_path, cocofake_path, transforms=transforms, split="train", limit=train_limit)
+    val = CocoFake(coco_path, cocofake_path, transforms=transforms, split="val", limit=val_limit, test_split=test_split)
+    test = CocoFake(coco_path, cocofake_path, transforms=transforms, split="test", limit=val_limit, test_split=test_split)
 
     train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size)
     val_dataloader = torch.utils.data.DataLoader(val, batch_size=batch_size)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size)
 
-    return train_dataloader, val_dataloader
+    return train_dataloader, val_dataloader, test_dataloader
 
--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -4,11 +4,15 @@ from tqdm.auto import tqdm
 from data.util.cocofake import get_cocofake
 
 if __name__ == "__main__":
-    train, val = get_cocofake("../data/coco-2014", "../data/cocofake", n=1000)
+    train, val, test, = get_cocofake("../data/coco-2014", "../data/cocofake", train_limit=1000, val_limit=500)
+    breakpoint()
     for batch in tqdm(train):
         pass
 
     for batch in tqdm(val):
         pass
 
-    print("Successfully iterated through train and validation sets.")
+    for batch in tqdm(test):
+        pass
+
+    print("Successfully iterated through train, validation, and test sets.")
