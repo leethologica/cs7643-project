cd1e891fb7ebbf7a518815d4971306eb52534d6c
Marcus Zheng Hao Tan <mtan75@login-ice-1.pace.gatech.edu>
2024-11-12 22:59:34 -0500

    updating squeezenet

--- data/util/cocofake.py
+++ data/util/cocofake.py
@@ -2,8 +2,37 @@ import os
 import json
 import torch
 import numpy as np
+import random
 
-from PIL import Image
+from PIL import Image, ImageOps
+
+# Marcus added a pad_to_square index to take the first fake image.
+def pad_to_square(image, target_size=(640, 640)):
+    """
+    Conduct padding on the image to make it 640 by 640
+    
+    Args:
+    - image: PIL Image object
+    - target_size: tuple (width, height), the desired size of the output image
+    
+    Returns:
+    - Padded PIL Image
+    """
+    # Get the original size of the image
+    width, height = image.size
+
+    # Calculate padding for left, right, top, and bottom
+    left = (target_size[0] - width) // 2
+    right = target_size[0] - width - left
+    top = (target_size[1] - height) // 2
+    bottom = target_size[1] - height - top
+
+
+    # Add padding to the image
+    padding_color = (0, 0, 0) if image.mode == "RGB" else 0 #There's some grayscale images, hence need to change to grayscale instead of a tuple.
+    padded_image = ImageOps.expand(image, (left, top, right, bottom), padding_color)
+
+    return padded_image
 
 class CocoFake(torch.utils.data.Dataset):
     def __init__(
@@ -15,13 +44,14 @@ class CocoFake(torch.utils.data.Dataset):
         split="train",
         limit=-1,
         test_split=None,
+        fake_prob = 0.2
     ):
         if split == "test" or split == "val":
-            self.cocofake_path = os.path.join(cocofake_path, "val2014")
-            self.coco_path = os.path.join(coco_path, "validation", "data")
+            self.cocofake_path = os.path.join(cocofake_path, "val2014") 
+            self.coco_path = os.path.join(coco_path, "val2014") #Updated with file path
         else:
             self.cocofake_path = os.path.join(cocofake_path, f"train2014")
-            self.coco_path = os.path.join(coco_path, "train", "data")
+            self.coco_path = os.path.join(coco_path, "train2014") #Updated with file path
 
         self.real_images = sorted(os.listdir(self.coco_path))[:limit]
         if test_split is not None:
@@ -30,7 +60,13 @@ class CocoFake(torch.utils.data.Dataset):
                 self.real_images = self.real_images[n:]
             else:
                 self.real_images = self.real_images[:n]
-        self.transforms = transforms
+
+        # self.transforms = transforms #commeting this out because this is not used - random variable initialized.
+        self.real_transform = real_transform
+        self.fake_transform = fake_transform
+
+        # Adding prob of fake images
+        self.fake_prob = fake_prob
 
     def __len__(self):
         return len(self.real_images)
@@ -38,24 +74,32 @@ class CocoFake(torch.utils.data.Dataset):
     def __getitem__(self, index):
         path = os.path.join(self.coco_path, self.real_images[index])
         img_id = os.path.basename(path).split(".")[0]
-        real_image = np.array(Image.open(path))
-        if len(real_image.shape) != 3:
-            real_image = np.array(Image.open(path).convert("RGB"))
+
+        real_image = Image.open(path).convert('RGB') #Marcus: Breakout real image into open image & array so that we can insert pad_to_square
+        real_image = pad_to_square(real_image, target_size=(640, 640)) # Marcus added pad_to_square    
+        real_image = np.array(real_image) # Converting to array
+
         fake_image_paths = sorted(os.listdir(os.path.join(self.cocofake_path, img_id)))
         fake_images = [
-            np.array(Image.open(os.path.join(self.cocofake_path, img_id, x)))
+            Image.open(os.path.join(self.cocofake_path, img_id, x)).convert('RGB')
             for x in fake_image_paths
-        ]
+        ] #Breaking out fake images into open and array
+        fake_images = [pad_to_square(img, target_size=(640, 640)) for img in fake_images] # Marcus added padding to the square image
+        fake_images = [np.array(img) for img in fake_images] # Converting into a list of arrays
 
         if self.real_transform is not None:
             real_image = self.real_transform(real_image)
         if self.fake_transform is not None:
-            fake_images = [self.fake_transform(x) for x in fake_images]
+            fake_images = [self.fake_transform(x) for x in fake_images] 
 
         real_image = np.transpose(real_image, (2, 0, 1)).astype(np.float32)
-        fake_images = [np.transpose(x, (2, 0, 1)).astype(np.float32) for x in fake_images]
+        fake_images = [np.transpose(x, (2, 0, 1)).astype(np.float32) for x in fake_images] 
 
-        return {"real": real_image, "fake": fake_images}
+        if random.random() > self.fake_prob:
+            fake_images[0] = np.zeros_like(real_image)
+
+        return {"real": real_image, "fake": fake_images[0]} # Marcus added a 0 index to take the first fake image. WE WILL CHANGE THIS IN THE FUTURE WHEN WE TRAIN ON UNBALANCED DATASET
+
 
 
 def get_cocofake(
@@ -66,6 +110,7 @@ def get_cocofake(
     train_limit=-1,
     val_limit=-1,
     test_split=0.5,
+    fake_prob = 0.2
 ):
     """
     Retrieve train, validation, and test dataloaders.
@@ -102,13 +147,12 @@ def get_cocofake(
 
     assert os.path.exists(cocofake_path)
 
-    train = CocoFake(coco_path, cocofake_path, transforms=transforms, split="train", limit=train_limit)
-    val = CocoFake(coco_path, cocofake_path, transforms=transforms, split="val", limit=val_limit, test_split=test_split)
-    test = CocoFake(coco_path, cocofake_path, transforms=transforms, split="test", limit=val_limit, test_split=test_split)
+    train = CocoFake(coco_path, cocofake_path, split="train", limit=train_limit, fake_prob = fake_prob) #removed: , transforms=transforms. added: fake_prob
+    val = CocoFake(coco_path, cocofake_path, split="val", limit=val_limit, test_split=test_split, fake_prob = 1) #removed: , transforms=transforms. added: fake_prob; val_loader should always be 1 to return all images
+    test = CocoFake(coco_path, cocofake_path, split="test", limit=val_limit, test_split=test_split, fake_prob = 1) #removed: , transforms=transforms added: fake_prob; val_loader should always be 1 to return all images
 
     train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size)
     val_dataloader = torch.utils.data.DataLoader(val, batch_size=batch_size)
     test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size)
 
     return train_dataloader, val_dataloader, test_dataloader
-
---
+++ models/squeezenet/train.py
@@ -0,0 +1,340 @@
+import torch
+import numpy as np
+import random
+import os
+from PIL import Image
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torchvision.models as models
+from torchvision.models import SqueezeNet1_1_Weights
+import torch.nn as nn
+import torch.optim as optim
+from cocofake import CocoFake, get_cocofake
+
+# Largest image 640 x 640
+
+# SEEDING FUNCTION:
+def set_seed(seed):
+    """
+    Set the seed for reproducibility across:
+    - Python's built-in random module
+    - Numpy
+    - PyTorch
+    - CUDA (if using a GPU)
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # For multi-GPU setups
+
+set_seed(42)
+
+######################################################################### TRAINING FOR BENCHMARK #########################################################################
+
+###### PART 1: DEFINING VARIABLES ######
+coco_path = '/home/hice1/mtan75/scratch/dlproject/dataset_real'
+cocofake_path = '/home/hice1/mtan75/scratch/dlproject/dataset_fake' # Path definitions
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Device definition
+
+batch_size = 256 # Defining the batch size for training
+train_limit = 10000 # Training only on a subset of dataset
+val_limit = 5000 # Validating only on a subset of validation dataset
+test_split = 0.5 # splitting up the val dataset into val and test
+fake_prob = 1 #the percentage of fake images to keep
+
+criterion = nn.CrossEntropyLoss() #Loss function
+learning_rate = 0.001
+
+num_epochs = 15
+
+
+print('Is Cuda available:', torch.cuda.is_available())
+print('Variables Defined!')
+
+###### PART 2: TRANSFORM & GENERATING DATA - Simple transformation to fit your model size. ######
+transforms = transforms.Compose([
+    transforms.Resize(224),  # Resize the image to 224x224 as required by Squeezenet
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  #Normalization used based on ImageNet's means/SD
+])
+
+# Getting dataset loaders
+train_loader, val_loader, test_loader = get_cocofake(
+    coco_path=coco_path,
+    cocofake_path=cocofake_path,
+    transforms=transforms, 
+    batch_size=batch_size,  
+    train_limit=train_limit,
+    val_limit=val_limit, 
+    test_split=test_split,
+    fake_prob = fake_prob  # Split validation data into test and validation sets
+)
+
+print('Dataloading/Transformation Completed!')
+
+###### PART 3: MODEL BUILDING & EVALUATION ######
+
+# Importing Squeezenets
+model = models.squeezenet1_1(weights=SqueezeNet1_1_Weights.DEFAULT)
+model.num_classes = 2
+
+# Updating the last layer of Squeenet
+model.classifier[1] = nn.Conv2d(512, 2, kernel_size=(1, 1), stride=(1, 1))  # 2 classes
+
+# Pushing model to GPU
+model = model.to(device)
+
+# Model optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+print('Model Defined!')
+
+# TRAINING FUNCTION!
+def train(model, train_loader, criterion, optimizer, device):
+    model.train()  # Set model to training mode
+    running_loss = 0.0
+    correct = 0
+    total=0
+
+    for batch in train_loader:
+
+        # Real image: creating labels
+        real_images = batch["real"].to(device)
+        real_labels = torch.zeros(real_images.size(0), dtype=torch.long).to(device)
+
+        # # Fake image & concatenating real + fake images if possible
+        # print(batch.keys())
+
+        # if batch.get('fake') is None:
+        #     inputs = real_images
+        #     labels = real_labels
+            
+        # else:
+        fake_images = batch["fake"].to(device)   
+        fake_labels = torch.ones(fake_images.size(0), dtype=torch.long).to(device)
+
+        # Concatenate images and labels to make a single batch
+        inputs = torch.cat((real_images, fake_images), dim=0)
+        labels = torch.cat((real_labels, fake_labels), dim=0)
+   
+        # print('labels here', labels)
+        # print('inptus here', inputs)
+
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        _, preds = torch.max(outputs, 1)
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * inputs.size(0)
+
+    epoch_loss = running_loss / len(train_loader.dataset)
+    accuracy = correct / total
+
+    return epoch_loss, accuracy
+
+# VALIDATION FUNCTION!
+def validate(model, val_loader, criterion, device):
+    model.eval()  # Set model to evaluation mode
+    running_loss = 0.0
+    correct = 0
+    total=0
+
+    with torch.no_grad():
+        for batch in val_loader:
+            real_images = batch["real"].to(device)
+            fake_images = batch["fake"].to(device)
+
+
+            real_labels = torch.zeros(real_images.size(0), dtype=torch.long).to(device)
+            fake_labels = torch.ones(fake_images.size(0), dtype=torch.long).to(device)
+
+            inputs = torch.cat((real_images, fake_images), dim=0)
+            labels = torch.cat((real_labels, fake_labels), dim=0)
+
+            outputs = model(inputs)
+            print(outputs)
+            # print(inputs.shape)
+            # print(inputs)
+
+            # print('outputs here', outputs)
+
+            # if torch.equal(outputs[1], torch.tensor([0.0, 0.0], device=device)):
+                
+            #     print('equal!')
+            #     print(inputs)
+            #     print(outputs)
+
+
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * inputs.size(0)
+
+            _, preds = torch.max(outputs, 1)
+
+            # print('predshere',preds)
+
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+    
+    epoch_loss = running_loss / total
+    accuracy = correct / total
+    return epoch_loss, accuracy
+
+
+###### PART 4: CHECKPOINTING/SEED SETTING ######
+
+# SAVING CHECKPOINT!
+def save_checkpoint(model, optimizer, epoch, train_loss, val_loss, dataloader_state = 'seed', filename="checkpoint.pth"):
+    checkpoint = {
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(), #Saving optimizer
+        "train_loss": train_loss,
+        "val_loss": val_loss,
+        "random_state": 42,  # Fixed at 42
+        "dataloader_state": dataloader_state #Fixing at seed 
+    }
+    torch.save(checkpoint, filename)
+    print(f"Checkpoint saved at epoch {epoch}") # We are saving by epochs not by bach size
+
+# LOADING CHECKPOINT!
+def load_checkpoint(model, optimizer, filename="checkpoint.pth", device="cuda"):
+    checkpoint = torch.load(filename, map_location=device)
+    
+    
+    # NEED TO REMOVE THIS IF YOU'RE USING MULTIPLE GPU. PROBLEM OF 'module.' BEING A PREFIX IN THE KEYS WHEN YOU USE DATAPARALLEL
+    state_dict = checkpoint["model_state_dict"]
+    new_state_dict = {}
+    
+    for key, value in state_dict.items():
+        new_key = key.replace("module.", "")  # Remove 'module.' prefix
+        new_state_dict[new_key] = value
+    
+    # Load the updated state dicitonaries
+    model.load_state_dict(new_state_dict)
+
+    # Loading the rest of the checkpoints
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    epoch = checkpoint["epoch"]
+    train_loss = checkpoint["train_loss"]
+    val_loss = checkpoint["val_loss"]
+    random_state = checkpoint["random_state"]
+    dataloader_state = checkpoint["dataloader_state"]
+    
+    print(f"Checkpoint loaded from epoch {epoch}")
+
+    set_seed(42)
+
+    return model, optimizer, epoch, train_loss, val_loss, random_state, dataloader_state
+
+
+###### PART 5: MODEL BUILDING & EVALUATION ######
+
+# Loading checkpoint in case of GPU FAILURE. TO BE UNCOMMENTED OUT WHEN YOU NEED TO LOAD CHECKPOINT!
+# model, optimizer, ep, train_loss, val_loss, random_state, dataloader_state = load_checkpoint(model, optimizer, "checkpoint.pth")
+try: # Setting this so as to use this for the range function in the training methods below.
+    print(ep)
+except:
+    ep = 0
+
+# Simple Parallelization
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs!")
+    model = torch.nn.DataParallel(model)
+
+print('Training Starts!')
+
+for epoch in range(ep, num_epochs):
+
+    train_loss, train_accuracy = train(model, train_loader, criterion, optimizer, device)
+    val_loss, val_accuracy = validate(model, val_loader, criterion, device)
+    
+    print(f"Completed Epoch {epoch + 1}/{num_epochs}")
+    print(f"Train Loss: {train_loss:.4f} | Train accuracy: {train_accuracy * 100:.2f}%")
+    print(f"Val Loss: {val_loss:.4f} | Val Accuracy: {val_accuracy * 100:.2f}%")
+
+    save_checkpoint(model, optimizer, epoch, train_loss, val_loss)
+
+# print(ep)
+
+# for name, param in model.state_dict().items():
+#     print(f"Layer: {name}, Weights: {param}")
+
+
+
+
+######################################################################### MISC SECTION - TESTING ON DATASIZE ETC. #########################################################################
+
+
+
+# Define the path to your folder with images
+# folder_path = "/home/hice1/mtan75/scratch/dlproject/dataset_real/train2014"  # Replace with your folder path
+# folder_path = '/home/hice1/mtan75/scratch/dlproject/dataset_fake/train2014/COCO_train2014_000000057300'
+
+
+# ctr = 1
+# # List all files in the folder
+# for filename in os.listdir(folder_path):
+#     # Construct the full file path
+#     file_path = os.path.join(folder_path, filename)
+
+#     # Check if it's an image file by its extension (optional but recommended)
+#     if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+#         try:
+#             # Open the image and get its dimensions
+#             with Image.open(file_path) as img:
+#                 width, height = img.size
+#                 print(f"Image: {filename}, Width: {width}, Height: {height}")
+#         except Exception as e:
+#             print(f"Could not open {filename}: {e}")
+#     ctr+=1
+
+
+# Initialize variables to track max dimensions
+# max_width = 0
+# max_height = 0
+# max_image = ''
+
+# ctr = 0
+# directory_path = '/home/hice1/mtan75/scratch/dlproject/dataset_real/train2014'
+
+# # Loop through all files in the directory
+# for filename in os.listdir(directory_path):
+#     if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+#         image_path = os.path.join(directory_path, filename)
+        
+#         try:
+#             # Open the image using PIL
+#             with Image.open(image_path) as img:
+#                 # Get image dimensions (width, height)
+#                 width, height = img.size
+#                 print(f"Image: {filename}, Width: {width}, Height: {height}")
+                
+#                 # Update max dimensions if the current image is larger
+#                 if width > max_width or height > max_height:
+#                     max_width = width
+#                     max_height = height
+#                     max_image = filename
+#         except Exception as e:
+#             print(f"Error opening {filename}: {e}")
+
+
+
+#     ctr +=1
+#     if ctr >= 40000:
+#         break
+
+# # Output the results
+# print(f"The largest image is {max_image} with dimensions: {max_width}x{max_height}")
+
