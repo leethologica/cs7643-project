5be1c89333962d0dd335d7d59b6269d20a0b6649
Marcus Tan <153584786+marcustan-mt@users.noreply.github.com>
2024-12-04 19:13:44 -0800

    Add files via upload

--- data/util/cocofake.py
+++ data/util/cocofake.py
@@ -3,6 +3,8 @@ import json
 import torch
 import numpy as np
 import random
+# import matplotlib.pyplot as plt
+
 
 from PIL import Image, ImageOps
 
@@ -27,7 +29,6 @@ def pad_to_square(image, target_size=(640, 640)):
     top = (target_size[1] - height) // 2
     bottom = target_size[1] - height - top
 
-
     # Add padding to the image
     padding_color = (0, 0, 0) if image.mode == "RGB" else 0 #There's some grayscale images, hence need to change to grayscale instead of a tuple.
     padded_image = ImageOps.expand(image, (left, top, right, bottom), padding_color)
@@ -39,8 +40,7 @@ class CocoFake(torch.utils.data.Dataset):
         self,
         coco_path,
         cocofake_path,
-        real_transform=None,
-        fake_transform=None,
+        transforms=None,
         split="train",
         limit=-1,
         test_split=None,
@@ -61,9 +61,8 @@ class CocoFake(torch.utils.data.Dataset):
             else:
                 self.real_images = self.real_images[:n]
 
-        # self.transforms = transforms #commeting this out because this is not used - random variable initialized.
-        self.real_transform = real_transform
-        self.fake_transform = fake_transform
+        # self.transforms = critical to ensuring good learning
+        self.transforms = transforms
 
         # Adding prob of fake images
         self.fake_prob = fake_prob
@@ -75,30 +74,37 @@ class CocoFake(torch.utils.data.Dataset):
         path = os.path.join(self.coco_path, self.real_images[index])
         img_id = os.path.basename(path).split(".")[0]
 
+        #### REAL IMAGES TRANSFORMS AND CONVERSION TO NUMPY ####
         real_image = Image.open(path).convert('RGB') #Marcus: Breakout real image into open image & array so that we can insert pad_to_square
         real_image = pad_to_square(real_image, target_size=(640, 640)) # Marcus added pad_to_square    
+
+        if self.transforms is not None:
+            real_image = self.transforms(real_image) #Transforms should come before converting to array 
         real_image = np.array(real_image) # Converting to array
 
+        #### FAKE IMAGES TRANSFORMS AND CONVERSION TO NUMPY ####
         fake_image_paths = sorted(os.listdir(os.path.join(self.cocofake_path, img_id)))
+        
         fake_images = [
             Image.open(os.path.join(self.cocofake_path, img_id, x)).convert('RGB')
             for x in fake_image_paths
         ] #Breaking out fake images into open and array
+        
         fake_images = [pad_to_square(img, target_size=(640, 640)) for img in fake_images] # Marcus added padding to the square image
+        
+        if self.transforms is not None:
+            fake_images = [self.transforms(x) for x in fake_images] #Transforms should come before converting to array 
+
         fake_images = [np.array(img) for img in fake_images] # Converting into a list of arrays
 
-        if self.real_transform is not None:
-            real_image = self.real_transform(real_image)
-        if self.fake_transform is not None:
-            fake_images = [self.fake_transform(x) for x in fake_images] 
-
-        real_image = np.transpose(real_image, (2, 0, 1)).astype(np.float32)
-        fake_images = [np.transpose(x, (2, 0, 1)).astype(np.float32) for x in fake_images] 
+        # CONVERSION TO NUMPY
+        real_image = np.transpose(real_image, (0, 1, 2)).astype(np.float32)
+        fake_images = [np.transpose(x, (0, 1, 2)).astype(np.float32) for x in fake_images] 
 
         if random.random() > self.fake_prob:
             fake_images[0] = np.zeros_like(real_image)
 
-        return {"real": real_image, "fake": fake_images[0]} # Marcus added a 0 index to take the first fake image. WE WILL CHANGE THIS IN THE FUTURE WHEN WE TRAIN ON UNBALANCED DATASET
+        return {"real": real_image, "fake": fake_images[1]} # Marcus added a 0 index to take the first fake image. WE WILL CHANGE THIS IN THE FUTURE WHEN WE TRAIN ON UNBALANCED DATASET
 
 
 
@@ -110,7 +116,8 @@ def get_cocofake(
     train_limit=-1,
     val_limit=-1,
     test_split=0.5,
-    fake_prob = 0.2
+    fake_prob = 0.2,
+    train_n_workers = 8
 ):
     """
     Retrieve train, validation, and test dataloaders.
@@ -147,12 +154,12 @@ def get_cocofake(
 
     assert os.path.exists(cocofake_path)
 
-    train = CocoFake(coco_path, cocofake_path, split="train", limit=train_limit, fake_prob = fake_prob) #removed: , transforms=transforms. added: fake_prob
-    val = CocoFake(coco_path, cocofake_path, split="val", limit=val_limit, test_split=test_split, fake_prob = 1) #removed: , transforms=transforms. added: fake_prob; val_loader should always be 1 to return all images
-    test = CocoFake(coco_path, cocofake_path, split="test", limit=val_limit, test_split=test_split, fake_prob = 1) #removed: , transforms=transforms added: fake_prob; val_loader should always be 1 to return all images
+    train = CocoFake(coco_path, cocofake_path, split="train", transforms = transforms, limit=train_limit, fake_prob = fake_prob) #removed: added: fake_prob
+    val = CocoFake(coco_path, cocofake_path, split="val", transforms = transforms, limit=val_limit, test_split=test_split, fake_prob = 1) #removed: added: fake_prob; val_loader should always be 1 to return all images
+    test = CocoFake(coco_path, cocofake_path, split="test", transforms = transforms, limit=val_limit, test_split=test_split, fake_prob = 1) #removed: added: fake_prob; val_loader should always be 1 to return all images
 
-    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size)
-    val_dataloader = torch.utils.data.DataLoader(val, batch_size=batch_size)
-    test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, num_workers = train_n_workers)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=batch_size, num_workers = train_n_workers)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=batch_size, num_workers = train_n_workers)
 
     return train_dataloader, val_dataloader, test_dataloader
